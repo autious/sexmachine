16be8dda15ad803ecf0fe4f401e9aa8a30adeb25
Lucas Gullbo <lucas.gullbo@live.se>
2020-02-01 17:38:56 +0100

    Background Animation

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -7,9 +7,12 @@ public class ChangeScene : MonoBehaviour
     [SerializeField] SpriteRenderer renderer;
     [SerializeField] Sprite[] rooms;
 
+    Vector3 ogScale;
+
     int currentScene = 0;
 
     private void Awake() {
+        ogScale = transform.localScale;
         NextScene();
     }
 
@@ -17,12 +20,16 @@ public class ChangeScene : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.S)) {
             NextScene();
         }
+
+        transform.localScale = Vector3.Lerp(transform.localScale, ogScale, 5 * Time.deltaTime);
+
     }
 
     public void NextScene() {
         if(currentScene < rooms.Length) {
             renderer.sprite = rooms[currentScene];
             currentScene++;
+            transform.localScale = new Vector3(1.2f, 0.8f, 1);
         }
     }
 }
