1d417a254750950697fe58830e759e2f3d76d4a7
Lucas Gullbo <lucas.gullbo@live.se>
2020-02-01 12:52:12 +0100

    Face Emotions
    Added emootions enum
    Accesible from FaceSystem.INSTANCE.SetEmotion

---
+++ Assets/CameraHead.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHead : MonoBehaviour
+{
+
+    Vector3 ogRot;
+    [SerializeField] float amount;
+    Vector3 rot;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ogRot = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot = Spring(rot, new Vector3(-Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"), 0) * amount);
+        transform.rotation = Quaternion.Euler(rot);
+    }
+
+    Vector3 calc = Vector3.zero;
+    Vector3 vel = Vector3.zero;     //The Launch! (Only for the begining to kinda give the object an extra push!)
+
+    [Header("Lerp")]
+    [SerializeField] float fric = 0.9f;//The friction, higher value = lower friction! (It's beacuse vel*=fric)
+    [SerializeField] Vector2 amountRange = new Vector2(0.1f, 0.2f); //The 'update speed', higher value = smoother!
+
+    Vector3 Spring(Vector3 myPos, Vector3 gotoPos) {
+
+        //X
+        vel.x *= fric;                          //Multiplys the Velocity with the friction!
+        vel.x += ((gotoPos.x - myPos.x) / Random.Range(amountRange.x, amountRange.y)) * Time.deltaTime; //Adds Velocity, from pos A to B, divides with Amount to make it smooth
+        calc.x += vel.x * Time.deltaTime;       //Adds the velocity to the x-position!
+
+        //Y
+        vel.y *= fric;                          //Multiplys the Velocity with the friction!
+        vel.y += ((gotoPos.y - myPos.y) / Random.Range(amountRange.x, amountRange.y)) * Time.deltaTime; //Adds Velocity, from pos A to B, divides with Amount to make it smooth
+        calc.y += vel.y * Time.deltaTime;        //Adds the velocity to the x-position!
+
+        //Z
+        vel.z *= fric;                          //Multiplys the Velocity with the friction!
+        vel.z += ((gotoPos.z - myPos.z) / Random.Range(amountRange.x, amountRange.y)) * Time.deltaTime; //Adds Velocity, from pos A to B, divides with Amount to make it smooth
+        calc.z += vel.z * Time.deltaTime;       //Adds the velocity to the x-position!
+
+        return calc;
+    }
+}
---
+++ Assets/FaceSystem.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FaceSystem : MonoBehaviour {
+
+    AudioSource aud;
+
+    public static FaceSystem INSTANCE;
+    private void Awake() { INSTANCE = this;
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        SetEmotion(Emotion.idle);
+    }
+
+    [SerializeField] RotationState[] eyebrow;
+    [SerializeField] BlinkSystem[] eyes;
+    [SerializeField] GameObject[] blush;
+    [SerializeField] GameObject[] tears;
+    [SerializeField] GameObject mad;
+
+    public enum Emotion { idle, happy, angry, sad, blush }
+
+    [SerializeField] UnityEvent eventIdle;
+    [SerializeField] UnityEvent eventHappy;
+    [SerializeField] UnityEvent eventAngry;
+    [SerializeField] UnityEvent eventSad;
+    [SerializeField] UnityEvent eventBlush;
+
+    public void SetEmotion(Emotion emotion) {
+        switch(emotion) {
+                case Emotion.idle:
+            SetBrow(0);
+            eventIdle.Invoke();
+            SetBlush(false);
+            SetTears(false);
+            SetMad(false);
+
+            break;
+                case Emotion.happy:
+            SetBrow(0);
+            eventHappy.Invoke();
+            SetBlush(false);
+            SetTears(false);
+            SetMad(false);
+
+            break;
+                case Emotion.angry:
+            SetBrow(1);
+            eventAngry.Invoke();
+            SetBlush(false);
+            SetTears(false);
+            SetMad(true);
+
+            break;
+                case Emotion.sad:
+            SetBrow(3);
+            eventSad.Invoke();
+            SetBlush(false);
+            SetTears(true);
+            SetMad(false);
+
+            break;
+                case Emotion.blush:
+            SetBrow(2);
+            eventBlush.Invoke();
+            SetBlush(true);
+            SetTears(false);
+            SetMad(false);
+
+            break;
+            default:
+            break;
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Alpha1)) { SetEmotion(Emotion.idle); }
+        if(Input.GetKeyDown(KeyCode.Alpha2)) { SetEmotion(Emotion.happy); }
+        if(Input.GetKeyDown(KeyCode.Alpha3)) { SetEmotion(Emotion.angry); }
+        if(Input.GetKeyDown(KeyCode.Alpha4)) { SetEmotion(Emotion.sad); }
+        if(Input.GetKeyDown(KeyCode.Alpha5)) { SetEmotion(Emotion.blush); }
+    }
+
+    public void SetEye(Sprite _spr) {
+        for(int i = 0; i < eyes.Length; i++) {
+            eyes[i].SetEmotion(_spr);
+        }
+    }
+
+    void SetBlush(bool _state) {
+        for(int i = 0; i < blush.Length; i++) {
+            blush[i].SetActive(_state);
+        }
+    }
+
+    void SetMad(bool _state) { mad.SetActive(_state); }
+
+    void SetTears(bool _state) {
+        for(int i = 0; i < tears.Length; i++) {
+            tears[i].SetActive(_state);
+        }
+    }
+
+    void SetBrow(int index) {
+        switch(index) {
+            //Angry
+            case 1:
+            for(int i = 0; i < eyebrow.Length; i++) {
+                eyebrow[i].rotationValue = -35;
+            }
+            break;
+            //Blush
+            case 2:
+            for(int i = 0; i < eyebrow.Length; i++) {
+                eyebrow[i].rotationValue = 15;
+            }
+            break;
+            //Sad
+            case 3:
+            for(int i = 0; i < eyebrow.Length; i++) {
+                eyebrow[i].rotationValue = 35;
+            }
+            break;
+            //Idle
+            default:
+            for(int i = 0; i < eyebrow.Length; i++) {
+                eyebrow[i].rotationValue = 0;
+            }
+            break;
+        }
+    }
+
+    public void PlaySound(AudioClip _clip) {
+        aud.pitch = Random.Range(0.6f, 1.1f);
+        aud.PlayOneShot(_clip);
+    }
+}
