15d4340d44c705b635cb4f45cbe0c2922fcf4a17
Lucas Gullbo <lucas.gullbo@live.se>
2020-02-01 10:22:26 +0100

    Face Assets

---
+++ Assets/BlinkSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkSystem : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer eye;
+    [SerializeField] Sprite eyeBlink;
+    Sprite lastFrame;
+
+    bool isBlinking;
+    float blinkTime;
+
+    Vector3 ogScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ogScale = transform.localScale;
+        DoBlink();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, ogScale, 9 * Time.deltaTime);
+
+        if(blinkTime > 0) {
+            blinkTime -= Time.deltaTime;
+        } else {
+            DoBlink();
+        }
+    }
+
+    void DoBlink() {
+        isBlinking = !isBlinking;
+        if(isBlinking) {
+            blinkTime = 0.2f;
+            lastFrame = eye.sprite;
+            eye.sprite = eyeBlink;
+
+            transform.localScale = new Vector3(1.2f, 0.8f, 1);
+
+        } else {
+            blinkTime = Random.Range(1.0f, 3.0f);
+            eye.sprite = lastFrame;
+
+            transform.localScale = new Vector3(0.8f, 1.2f, 1);
+
+        }
+    }
+
+}
---
+++ Assets/SpeakSystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakSystem : MonoBehaviour
+{
+    [SerializeField] float openTime = 0.1f;
+
+    [SerializeField] SpriteRenderer mouth;
+    [SerializeField] Sprite mouthOpen;
+    Sprite lastFrame;
+
+
+    [SerializeField] Transform mainTransform;
+
+    bool isSpeaking;
+    float speakTime;
+
+    Vector3 ogScale;
+
+    // Start is called before the first frame update
+    void Start() {
+        ogScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.localScale = Vector3.Lerp(transform.localScale, ogScale, 9 * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            //Speak
+            DoSpeak();
+        }
+
+        if(isSpeaking) {
+            if(speakTime > 0) {
+                speakTime -= Time.deltaTime;
+            } else {
+                isSpeaking = false;
+                mouth.sprite = lastFrame;
+                transform.localScale = new Vector3(0.8f, 1.2f, 1);
+
+            }
+        }
+    }
+
+    void DoSpeak() {
+        speakTime = openTime;
+        mainTransform.position -= Vector3.up * 0.2f;
+
+        if(!isSpeaking) {
+
+            isSpeaking = true;
+            lastFrame = mouth.sprite;
+            mouth.sprite = mouthOpen;
+            transform.localScale = new Vector3(1.2f, 0.8f, 1);
+
+        }
+    }
+}
