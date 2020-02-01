36f90f5f6b1a8a20585741e4cfc655c2f9b6e26b
LKTheDev <maxen940393@gmail.com>
2020-02-01 15:52:22 +0100

    Answerable questions

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public enum PlayerAnswer
 {
+    Nothing,
     Yes,
     No
 }
@@ -12,15 +13,112 @@ public enum PlayerAnswer
 [CreateAssetMenu(menuName = "Dialogue")]
 public class Question : TalkingElement
 {
-    int currentLement;
+    bool allDone;
+    int questionState = 0;
+    int currentElement = 0;
     public List<string> breadText;
-    public PlayerAnswer inExpectedAnswer;
+    public PlayerAnswer expectedAnswer;
     public List<string> wrongAnswer;
     public List<string> rightAnswer;
 
+    public Question(List<string> inBreadText, PlayerAnswer inExpectedAnswer, List<string> inWrongAnswer, List<string> inRightAnswer)
+    {
+        breadText = inBreadText;
+        this.expectedAnswer = inExpectedAnswer;
+        this.wrongAnswer = inWrongAnswer;
+        this.rightAnswer = inRightAnswer;
+    }
 
     public override string GetText()
     {
-        return "";// breadText.;
+     switch(questionState)
+        {
+            case 0:
+                return breadText[currentElement] + ":" + currentElement;
+                break;
+            case 1:
+                return breadText[currentElement] + ":" + currentElement;
+                break;
+            case 2:
+                return wrongAnswer[currentElement] + ":" + currentElement;
+                break;
+            case 3:
+                return rightAnswer[currentElement] + ":" + currentElement;
+                break;
+        }
+        return "Don't go here";
+    }
+
+    public override bool GoNext()
+    {
+        switch (questionState)
+        {
+            case 0:
+                if (currentElement < breadText.Count)
+                {
+                    if (InputManager.PushToTalk())
+                    {
+                        currentElement++;
+                    }
+                }
+                else
+                {
+                    currentElement = 0;
+                    questionState = 1;
+                }
+                break;
+            case 1:
+
+                PlayerAnswer answer = InputManager.yesAndNo.GetAnswer();
+
+                if(answer != PlayerAnswer.Nothing)
+                {
+                    if (expectedAnswer != answer)
+                    {
+                        questionState = 2;
+                    }
+                    else
+                    {
+                        questionState = 3;
+                    }
+                }
+
+                break;
+            case 2:
+                //WRONG ANSWER
+                if (currentElement < wrongAnswer.Count)
+                {
+                    if (InputManager.PushToTalk())
+                    {
+                        currentElement++;
+                    }
+                }
+                else
+                {
+                    currentElement = 0;
+                    allDone = true;
+                }
+                break;
+            case 3:
+                //RIGHT ANSWER
+                if (currentElement < rightAnswer.Count)
+                {
+                    if (InputManager.PushToTalk())
+                    {
+                        currentElement++;
+                    }
+                }
+                else
+                {
+                    currentElement = 0;
+                    allDone = true;
+                }
+                break;
+        }
+
+
+           
+
+        return allDone;
     }
 }
