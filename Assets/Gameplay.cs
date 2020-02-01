c49fec8b0698674b4949f4fa0e6fff3636602dfe
BowlOfEels <eels@void.void>
2020-02-01 15:52:22 +0100

    Answerable questions

--- Assets/Gameplay.cs
+++ Assets/Gameplay.cs
@@ -8,10 +8,16 @@ using UnityEngine.UI;
 
 public class TalkingElement : ScriptableObject
 {
+    public TalkingElement rightAnswerNode;
+    public TalkingElement wrongAnswerNode;
 
     public virtual string GetText()
     {
         return "";
+    }
+    public virtual bool GoNext()
+    {
+        return false;
     }
 }
 
@@ -28,6 +34,11 @@ public class RegularTalkingPoint: TalkingElement
     {
         return Text;
     }
+
+    public override bool GoNext()
+    {
+        return InputManager.PushToTalk();
+    }
 }
 
 public static class AndroidStatus
@@ -38,7 +49,7 @@ public static class AndroidStatus
 
     public static List<TalkingElement> talkingPoints = new List<TalkingElement>();
 
-    public static void AddTalkingElementy(TalkingElement inElement)
+    public static void AddTalkingElement(TalkingElement inElement)
     {
         talkingPoints.Add(inElement);
     }
@@ -58,9 +69,6 @@ public static class AndroidStatus
 
 public class Gameplay : MonoBehaviour
 {
-    public float inputXOut;
-    public float inputYOut;
-
     public Text outputDia;
 
     public Question[] questions;
@@ -90,11 +98,10 @@ public class Gameplay : MonoBehaviour
     void Update()
     {
         androidState.Update();
-        inputXOut = InputManager.GetX();
-        inputYOut = InputManager.GetY();
+
         outputDia.text = readLine;
 
-        if (!currentTalkingElement || InputManager.PushToTalk())
+        if (!currentTalkingElement || currentTalkingElement.GoNext())
             currentTalkingElement = AndroidStatus.GetTalkingElement();
 
         if (currentTalkingElement)
@@ -103,14 +110,8 @@ public class Gameplay : MonoBehaviour
                 SetDialogue(currentTalkingElement.GetText());
         }
 
-
-
         readLine = ReadCurrentDialogue();
-        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
-        {
-            if(Input.GetKeyDown(kcode))
-                Debug.Log("KeyCode down: " + kcode);
-        }
+
     }
 
     public void ChangeState(AndroidState changeToState)
@@ -174,13 +175,30 @@ public class AndroidUpset: AndroidState
 {
     public override void Enter()
     {
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("So..."));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("I think we need to talk"));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint(" Do you have no shame in your body"));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("The way that you've been treating me is not acceptable"));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("I want you to be honest with me and answer my questions with yes or no"));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("Wiggle my joystick back and forward for yes"));
-        AndroidStatus.AddTalkingElementy(new RegularTalkingPoint("And side to side for no"));
+        AndroidStatus.AddTalkingElement(new RegularTalkingPoint("So..."));
+        AndroidStatus.AddTalkingElement(new RegularTalkingPoint("I think we need to talk"));
+        AndroidStatus.AddTalkingElement(new RegularTalkingPoint(" Do you have no shame in your body"));
+        AndroidStatus.AddTalkingElement(new RegularTalkingPoint("The way that you've been treating me is not acceptable"));
+        AndroidStatus.AddTalkingElement(new RegularTalkingPoint("I want you to be honest with me and answer my questions with yes or no"));
+
+        Question tempQuestion;
+
+ 
+
+        List<string> questionBreadText = new List<string>();
+
+        questionBreadText.Add("Wiggle my joystick back and forward for yes");
+        questionBreadText.Add("And side to side for no");
+        questionBreadText.Add("Do you understand?");
+
+        List<string> wrongText = new List<string>();
+        wrongText.Add("Butthole Idiot");
+        List<string> righttext = new List<string>();
+        righttext.Add("THAT'S GOOD");
+        tempQuestion = new Question(questionBreadText, PlayerAnswer.Yes,wrongText,righttext);
+        tempQuestion.wrongAnswerNode = tempQuestion;
+
+        AndroidStatus.AddTalkingElement(tempQuestion);
     }
 
     public override void Update()
@@ -200,6 +218,8 @@ public class AndroidUpset: AndroidState
 
 public static class InputManager
 {
+    public static YesAndNo yesAndNo = new YesAndNo();
+
     public static float GetX()
     {
         return Input.GetAxis("Vertical");
@@ -220,8 +240,79 @@ public static class InputManager
         return Input.GetAxis("Vertical") < 0;
     }
 
+    public static PlayerAnswer answer()
+    {
+        return yesAndNo.GetAnswer();// Input.GetAxis("Vertical") < 0;
+    }
+
     public static bool PushToTalk()
     {
         return Input.GetKeyDown(KeyCode.Joystick2Button0);
+    }
+}
+
+public class YesAndNo
+{
+    int shakeCount;
+    bool isHorizontal;
+    float direction,lastDirection;
+
+    float stayStileTime;
+    public YesAndNo()
+    {
+        shakeCount = 0;
+        isHorizontal = true;
+        stayStileTime = 0;
+    }
+
+    public PlayerAnswer GetAnswer()
+    {
+
+        Debug.Log(shakeCount);
+        if (Math.Abs(InputManager.GetX()) <= 0.0f && Math.Abs(InputManager.GetY()) <= 0.0f )
+        {
+            stayStileTime += Time.deltaTime;
+            if (stayStileTime > 1)
+            {
+                shakeCount = 0;
+                Debug.Log("WABABABA");
+            }
+        }
+        else
+        {
+            stayStileTime = 0;
+            // Debug.Log("IN HERE");
+            //YES
+            if (Math.Abs(InputManager.GetX()) > Math.Abs(InputManager.GetY()))
+            {
+                if (!isHorizontal)
+                    shakeCount = 0;
+
+                isHorizontal = true;
+                direction = InputManager.GetX();
+
+                if (shakeCount >= 8)
+                    return PlayerAnswer.No;
+            }
+            //NO
+            else
+            {
+                if (isHorizontal)
+                    shakeCount = 0;
+                isHorizontal = false;
+                direction = InputManager.GetY();
+
+                if (shakeCount >= 8)
+                    return PlayerAnswer.Yes;
+            }
+
+            if ((direction + lastDirection) == 0)
+                shakeCount++;
+
+            lastDirection = direction;
+
+        }
+
+        return PlayerAnswer.Nothing;
     }
 }
